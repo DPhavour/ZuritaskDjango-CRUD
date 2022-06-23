afe513d1043eaa60c0b325133c138eaeb065f185
dphavour <divinephavor@gmail.com>
2022-06-23 09:48:26 +0100

    Create Views

--- I4G013177KBA/blog/views.py
+++ I4G013177KBA/blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from dataclasses import fields
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+#import built in class based view
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+
+# import the model Post from models.py
+from .models import Post
+
+# Create your views here.
+  
+  # Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
